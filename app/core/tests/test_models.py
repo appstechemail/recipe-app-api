2c3994791f8dcda45d48c527960bf8eff93d8cc4
appstechemail <appstechemail@gmail.com>
2024-10-18 23:51:44 +0530

    setup Django Admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,3 +29,18 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a value Error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'admin@admin.com',
+            'admin123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
