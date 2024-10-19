e35a12cb66caf7238f412ee02f96cfb8d0f830b5
appstechemail <appstechemail@gmail.com>
2024-10-19 13:25:25 +0530

    Add API documentation using drf_spectacular - changed urls.py code

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path(
+        'api/docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
